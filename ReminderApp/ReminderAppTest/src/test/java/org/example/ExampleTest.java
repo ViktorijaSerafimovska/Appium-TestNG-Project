98bbfb1c3ad11c5dd2ed56a4c8460f8ac4b7d7ea
ViktorijaSerafimovska <138993499+ViktorijaSerafimovska@users.noreply.github.com>
2025-05-05 23:18:33 +0200

    Add files via upload

---
+++ ReminderApp/ReminderAppTest/src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void sampleTest() {
+        System.out.println("Appium test goes here!");
+    }
+
+    @Test
+    public void addReminderTest() {
+        System.out.println("Opening Reminder App...");
+        System.out.println("Clicking on 'Add Reminder' button...");
+        System.out.println("Entering title: 'Buy groceries'");
+        System.out.println("Setting time: 'Thursday at 10:30 AM'");
+        System.out.println("Saving the reminder...");
+        System.out.println("Reminder added successfully!");
+    }
+}
